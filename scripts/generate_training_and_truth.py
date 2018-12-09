463a769a1b460714c088111737d05c4d2b2ccfc6
Omar Ali <524542+omar711@users.noreply.github.com>
2018-12-09 17:00:54 +0800

    Convert bounding boxes into lat/lon pairs and add a check for buildings wholly contained within a single map tile

--- scripts/generate_training_and_truth.py
+++ scripts/generate_training_and_truth.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser
+from util import bingmaps
 from pathlib import Path
 from util import bingmaps
 
@@ -25,6 +26,28 @@ def load_buildings_from_file(buildingCsv):
                     "bounding_box" : tokens[3:]
                 }
     
+
+def bounding_box_to_lat_lon_list(bounding_box):
+    box = []
+    for i in range(0, len(bounding_box), 2):
+        (lat, lon) = float(bounding_box[i]), float(bounding_box[i+1])
+        box.append([lat, lon])
+    return box
+
+
+def bounding_box_entirely_in_same_tile(bounding_box_lat_lon, zoom_level):
+    previous_quadkey = None
+
+    for (lat, lon) in bounding_box_lat_lon:
+        quadkey = bingmaps.quadkey_containing_lat_lon(lat, lon, zoom_level)
+
+        if previous_quadkey == quadkey or previous_quadkey is None:
+            previous_quadkey = quadkey
+        else:
+            return False
+
+    return True
+
 
 def get_output_folder(project_id, task_id, zoom_level, output_folder):
     path = Path(output_folder) / Path(str(project_id)) / Path(str(task_id)) / Path(str(zoom_level))
@@ -58,9 +81,12 @@ if __name__ == "__main__":
         project_id = task["project_id"]
         task_id = task["task_id"]
         way_id = task["way_id"]
-        bounding_box = task["bounding_box"]
+        bounding_box = bounding_box_to_lat_lon_list(task["bounding_box"])
 
-        print(project_id, task_id, way_id, bounding_box)
+        if bounding_box_entirely_in_same_tile(bounding_box, zoom_level):
+            print("IN")
+        else:
+            print("OUT")
         #output_folder = get_output_folder(project_id, task_id, zoom_level, args.outputFolder)
 
 
